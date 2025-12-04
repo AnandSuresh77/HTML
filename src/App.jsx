e4329a308a501ca9f100b23c835a02cd5da7ddc6
muhammedfahadss <fahadfadhuzz@gmail.com>
2025-12-04 12:51:53 +0530

    adding a new blue button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [color1, setcolor1] = useState("white");
+  function handleBlue() {
+    setcolor1("blue");
+  }
   return (
     <>
       <div>
@@ -21,6 +24,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={handleBlue} style={{ backgroundColor: color1 }}>
+          Blue
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
