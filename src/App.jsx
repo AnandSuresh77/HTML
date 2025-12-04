7cc4ce2ee5fe92535384812c8e35a3c47f52cf4a
Sivadeth-S-B-Nair <sivadeth2003@gmail.com>
2025-12-04 12:49:41 +0530

    adding a new cyan button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [color8, setColor8] = useState("white");
+  function handleCyan() {
+    setColor8("cyan");
+  }
   return (
     <>
       <div>
@@ -21,6 +24,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={handleCyan} style={{ backgroundColor: color8 }}>
+          Cyan
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
